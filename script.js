41eef469c2ff3aca47fbcad8524f85ceff5c9f32
Horacio-98 <85646036+Horacio-98@users.noreply.github.com>
2024-04-02 02:31:09 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const siBtn = document.getElementById('siBtn');
+const noBtn = document.getElementById('noBtn');
+
+noBtn.addEventListener('mouseover', function() {
+    const randomPositionX = Math.random() * (window.innerWidth - 100);
+    const randomPositionY = Math.random() * (window.innerHeight - 50);
+    
+    noBtn.style.position = 'absolute';
+    noBtn.style.left = randomPositionX + 'px';
+    noBtn.style.top = randomPositionY + 'px';
+});
+
+
+
+siBtn.addEventListener('click', function() {
+    const eleccion = siBtn.textContent; // Obtener el texto del botón "Sí"
+    mensaje.innerHTML = `¡Muy buena elección tomada!<br>Usted ha tomado la decisión de "${eleccion}"`;
+    mensaje.style.display = 'block';
+});
