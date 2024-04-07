b65f1eba77f0b0d0f3adbf8b337ba37b51cd8cb6
Horacio-98 <85646036+Horacio-98@users.noreply.github.com>
2024-04-06 22:05:08 -0600

    New cam

--- script.js
+++ script.js
@@ -14,12 +14,12 @@ noBtn.addEventListener('mouseover', function() {
 
 siBtn.addEventListener('click', function() {
     const eleccion = siBtn.textContent; // Obtener el texto del botón "Sí"
-    mensaje.innerHTML = `¡Muy buena elección tomada! 😜<br>Ya tomaste la decisión de "${eleccion}"<br><br>📸Favor de enviar foto 📸`;
+    mensaje.innerHTML = `¡Muy buena elección tomada! 😜<br>Ya tomaste la decisión de "${eleccion}"<br><br>📸 Favor de enviar foto 📸`;
     mensaje.style.display = 'block';
 });
 
 sipBtn.addEventListener('click', function() {
     const eleccion = sipBtn.textContent; // Obtener el texto del botón "Sí"
-    mensaje.innerHTML = `¡Muy buena elección tomada! 😜<br>Ya tomaste la decisión de "${eleccion}"<br><br>📸Favor de enviar foto 📸`;
+    mensaje.innerHTML = `¡Muy buena elección tomada! 😜<br>Ya tomaste la decisión de "${eleccion}"<br><br>📸 Favor de enviar foto 📸`;
     mensaje.style.display = 'block';
 });
